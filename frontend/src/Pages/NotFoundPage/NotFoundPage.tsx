7fcabffd3e7ca37ceceefb97ad9391dc85157f22
William Mattsson <wmattsson@hotmail.com>
2024-05-21 16:10:53 +0200

    Styled the not found page

--- frontend/src/Pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,26 @@
 import { Link } from 'react-router-dom'
+import Navbar from '../../Components/Navbar/Navbar'
+import './NotFoundPage.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const NotFoungPage = () => {
     return (
         <div>
-            <h1>Sorry, the page you are looking for could not be found.</h1>
-            <Link to="/">Go back to the homepage</Link>
+            <Navbar />
+            <div className="container">
+                <FontAwesomeIcon
+                    className="error-icon"
+                    icon={faCircleExclamation}
+                />
+                <h2>Sorry, the page you are looking for could not be found.</h2>
+                <p>Go back to the homepage</p>
+                <button>
+                    <Link className="btn-link" to="/">
+                        Home
+                    </Link>
+                </button>
+            </div>
         </div>
     )
 }
