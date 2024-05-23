8f51a3dce3f2b41e92d198f9147232a4d19db36f
William Mattsson <wmattsson@hotmail.com>
2024-05-23 10:34:04 +0200

    Styled product page

--- frontend/src/Components/ProductDetails/ProductDetails.tsx
+++ frontend/src/Components/ProductDetails/ProductDetails.tsx
@@ -12,10 +12,36 @@ interface Product {
 const ProductDetails = ({ product }: { product: Product }) => {
     return (
         <div className="product-details">
-            <h1>Produkt id - {product.id}</h1>
-            <img src={product.image} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>{product.price} kr</p>
+            <div className="product-details-img-container">
+                <img src={product.image} alt={product.name} />
+            </div>
+            <div className="product-details-text-container">
+                <h2>{product.name}</h2>
+                <h4>
+                    {product.category.toUpperCase()} -{' '}
+                    {product.gender.toUpperCase()}
+                </h4>
+                <div className="sizes">
+                    <h4>Sizes:</h4>
+                    <p>XS</p>
+                    <p>S</p>
+                    <p>M</p>
+                    <p>L</p>
+                    <p>XL</p>
+                </div>
+                <div className="product-details-text-info">
+                    <h5>Product Info</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed et felis metus. Sed in purus nec purus feugiat
+                        elementum. Nullam nec purus nec purus feugiat elementum.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Itaque nostr
+                    </p>
+                </div>
+                <h6>{product.price} kr</h6>
+                <button className="add-to-cart-btn">Add to cart</button>
+            </div>
         </div>
     )
 }
--- frontend/src/Pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ frontend/src/Pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -21,8 +21,8 @@ const ProductDetailsPage = () => {
         {
             id: 123,
             name: 'Product 1',
-            price: 100,
-            gender: 'men',
+            price: 120,
+            gender: 'woman',
             category: 'tops',
             image: 'https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
