e2e9c3ad0c776e9f5b7ae195966893ad59759fdb
William Mattsson <wmattsson@hotmail.com>
2024-05-26 16:30:29 +0200

    Added accordion as promo box and styled it

--- frontend/src/Components/CartPromoBox/CartPromoBox.tsx
+++ frontend/src/Components/CartPromoBox/CartPromoBox.tsx
@@ -1,6 +1,5 @@
-import './CartPromoBox.css'
+/* import './CartPromoBox.css'
 
-/* Att man kan trycka så öppnas den. Tänker liknande styling som checkout btn */
 const CartPromoBox = () => {
     return (
         <>
@@ -13,6 +12,53 @@ const CartPromoBox = () => {
                 <button className="cart-promo-button">Apply</button>
             </div>
         </>
+    )
+}
+
+export default CartPromoBox */
+
+import { useState } from 'react'
+import './CartPromoBox.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+const CartPromoBox = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [discountCode, setDiscountCode] = useState<string>('')
+
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDiscountCode(e.target.value)
+    }
+
+    const handleApplyCode = () => {
+        console.log(discountCode)
+        setDiscountCode('')
+    }
+
+    return (
+        <div className="cart-promo-box">
+            <div className="accordion-header" onClick={toggleAccordion}>
+                <span>Have a promo code?</span>
+                <span className={`promo-arrow ${isOpen ? 'open' : ''}`}>
+                    <FontAwesomeIcon icon={faArrowDown} />
+                </span>
+            </div>
+            {isOpen && (
+                <div className="accordion-content">
+                    <input
+                        type="text"
+                        value={discountCode}
+                        onChange={handleInputChange}
+                        placeholder="Enter promo code"
+                    />
+                    <button onClick={handleApplyCode}>Apply</button>
+                </div>
+            )}
+        </div>
     )
 }
 
