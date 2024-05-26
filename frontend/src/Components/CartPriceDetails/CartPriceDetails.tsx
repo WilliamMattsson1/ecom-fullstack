b374b9be1abe4b6dc7d339b14e715fb48363dcff
William Mattsson <wmattsson@hotmail.com>
2024-05-26 16:10:56 +0200

    Styled price details

--- frontend/src/Components/CartPriceDetails/CartPriceDetails.tsx
+++ frontend/src/Components/CartPriceDetails/CartPriceDetails.tsx
@@ -3,19 +3,19 @@ import './CartPriceDetails.css'
 const CartPriceDetails = () => {
     return (
         <div className="cart-price-box">
-            <div className="cart-price-details">
-                <p>Subtotal</p>
-                <p>$802</p>
+            <h2 className="cart-price-detail-title">Price Details</h2>
+            <div className="cart-price-detail">
+                <p className="cart-price-detail-left">Subtotal</p>
+                <p className="cart-price-detail-right">$802</p>
             </div>
-            <div className="cart-price-details">
-                <p>Shipping</p>
-                <p>$10</p>
+            <div className="cart-price-detail">
+                <p className="cart-price-detail-left">Shipping</p>
+                <p className="cart-price-detail-right">$0</p>
             </div>
-            <div className="cart-price-details">
-                <p>Total</p>
-                <p>$812</p>
+            <div className="cart-price-detail total-sum">
+                <p className="cart-price-detail-left">Total</p>
+                <p className="cart-price-detail-right">$802</p>
             </div>
-            <button className="cart-price-button">Checkout</button>
         </div>
     )
 }
