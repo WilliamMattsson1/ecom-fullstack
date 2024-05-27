76d27a8fdd93e1723155075d09d5d0204bbe23dc
William Mattsson <wmattsson@hotmail.com>
2024-05-27 16:48:44 +0200

    Added products from db to genderproducts

--- frontend/src/Pages/GenderProducts/GenderProducts.tsx
+++ frontend/src/Pages/GenderProducts/GenderProducts.tsx
@@ -4,155 +4,17 @@ import Navbar from '../../Components/Navbar/Navbar'
 import OfferBanner from '../../Components/OfferBanner/OfferBanner'
 import ProductItem from '../../Components/ProductItem/ProductItem'
 import './GenderProducts.css'
+import useProducts from '../../Context/useProducts'
 
 interface GenderProductsProps {
     gender: string
 }
 
-/* Ska inhämta produkter från parametern props.gender och visa alla produkter med det gender */
 const GenderProducts = (props: GenderProductsProps) => {
-    interface Product {
-        id: number
-        name: string
-        price: number
-        gender: string
-        category: string
-        image: string
-    }
-
-    const products: Product[] = [
-        {
-            id: 5,
-            name: 'Mens T-shirt Military Green Long Sleeve',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 6,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 7,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 8,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 9,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 10,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 11,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 12,
-            name: 'Mens T-shirt',
-            price: 200,
-            gender: 'men',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 13,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 14,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 15,
-            name: 'Woman T-shirt White',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 16,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 17,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 18,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 19,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        },
-        {
-            id: 20,
-            name: 'Woman T-shirt',
-            price: 200,
-            gender: 'woman',
-            category: 't-shirt',
-            image: 'https://images.unsplash.com/photo-1552642986-ccb41e7059e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHwy'
-        }
-    ]
+    const { allProducts } = useProducts()
 
     /* Get array with the right gender */
-    const genderProducts = products.filter(
+    const genderProducts = allProducts.filter(
         (product) => product.gender === props.gender
     )
 
