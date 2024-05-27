cbfcd3869a8f3a6d9468ee9df148d26a20050ae3
William Mattsson <wmattsson@hotmail.com>
2024-05-27 16:38:22 +0200

    Connected Trending products with db products via useProducts context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import HeroSection from './Components/HeroSection/HeroSection'
@@ -6,29 +5,7 @@ import TrendingProducts from './Components/TrendingProducts/TrendingProducts'
 import InfoBox from './Components/InfoBox/InfoBox'
 import NewsletterSub from './Components/NewsletterSub/NewsletterSub'
 
-interface Product {
-    id: number
-    name: string
-    price: number
-}
-
 function App() {
-    const [products, setProducts] = useState<Product[]>([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/products')
-                const data = await response.json()
-                setProducts(data)
-            } catch (error) {
-                console.error('Error fetching data:', error)
-            }
-        }
-
-        fetchData()
-    }, [])
-
     return (
         <>
             <Navbar />
@@ -37,14 +14,6 @@ function App() {
             <InfoBox />
             <NewsletterSub />
             <Footer />
-
-            {/* Enbart för att kolla så att backend och frontend kan kommunicera  */}
-            {products.map((product) => (
-                <div key={product.id}>
-                    <h2>{product.name}</h2>
-                    <p>{product.price} kr</p>
-                </div>
-            ))}
         </>
     )
 }
--- frontend/src/Components/TrendingProducts/TrendingProducts.tsx
+++ frontend/src/Components/TrendingProducts/TrendingProducts.tsx
@@ -1,51 +1,13 @@
 import ProductItem from '../ProductItem/ProductItem'
 import DownArrow from '../DownArrow/DownArrow'
 import './TrendingProducts.css'
+import useProducts from '../../Context/useProducts'
 
 const TrendingProducts = () => {
-    interface Product {
-        id: number
-        name: string
-        price: number
-        gender: string
-        category: string
-        image: string
-    }
+    const { allProducts } = useProducts()
+    const trendingProducts = allProducts.slice(0, 4)
+    console.log('Trending products (4)', trendingProducts)
 
-    const products: Product[] = [
-        {
-            id: 1,
-            name: 'Product 1',
-            price: 100,
-            gender: 'woman',
-            category: 'tops',
-            image: 'https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        },
-        {
-            id: 2,
-            name: 'Product 2',
-            price: 200,
-            gender: 'woman',
-            category: 'tops',
-            image: 'https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        },
-        {
-            id: 3,
-            name: 'Product 3',
-            price: 300,
-            gender: 'woman',
-            category: 'tops',
-            image: 'https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        },
-        {
-            id: 4,
-            name: 'Product 4',
-            price: 400,
-            gender: 'woman',
-            category: 'tops',
-            image: 'https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        }
-    ]
     return (
         <>
             <div className="trending-products-container">
@@ -55,7 +17,7 @@ const TrendingProducts = () => {
                 </p>
                 <hr className="hr-1" />
                 <div className="trending-products">
-                    {products.map((product) => {
+                    {trendingProducts.map((product) => {
                         return (
                             <ProductItem
                                 key={product.id}
