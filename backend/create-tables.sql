71049347a5d8f8520b6cada94a0ea1591321fa6e
William Mattsson <wmattsson@hotmail.com>
2024-05-27 15:13:37 +0200

    Added code for creating products table

--- backend/create-tables.sql
+++ backend/create-tables.sql
@@ -3,14 +3,11 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    price INTEGER NOT NULL
+    price INTEGER NOT NULL,
+    gender TEXT NOT NULL,
+    category TEXT NOT NULL,
+    image TEXT NOT NULL
 );
-
-INSERT INTO products (name, price) VALUES ('T-shirt', 19.99);
-INSERT INTO products (name, price) VALUES ('Jeans', 49.99);
-INSERT INTO products (name, price) VALUES ('Jacket', 89.99);
-INSERT INTO products (name, price) VALUES ('Sneakers', 59.99);
-
 
 CREATE TABLE IF NOT EXISTS newsletterSubs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
