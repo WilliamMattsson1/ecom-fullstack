bba5ccf7046643904735c93303fd7ff53a89d42d
William Mattsson <wmattsson@hotmail.com>
2024-05-29 11:04:06 +0200

    Showing total cart price and added shipping default

--- frontend/src/Components/CartPriceDetails/CartPriceDetails.tsx
+++ frontend/src/Components/CartPriceDetails/CartPriceDetails.tsx
@@ -1,20 +1,31 @@
 import './CartPriceDetails.css'
+import { useContext } from 'react'
+import { CartContext } from '../../Context/CartContext'
 
 const CartPriceDetails = () => {
+    const { getTotalCartAmount } = useContext(CartContext) || {
+        getTotalCartAmount: () => 0
+    }
+    const shipping: number = 2.99
+
     return (
         <div className="cart-price-box">
             <h2 className="cart-price-detail-title">Price Details</h2>
             <div className="cart-price-detail">
                 <p className="cart-price-detail-left">Subtotal</p>
-                <p className="cart-price-detail-right">$802</p>
+                <p className="cart-price-detail-right">
+                    {getTotalCartAmount()}
+                </p>
             </div>
             <div className="cart-price-detail">
                 <p className="cart-price-detail-left">Shipping</p>
-                <p className="cart-price-detail-right">$0</p>
+                <p className="cart-price-detail-right">${shipping}</p>
             </div>
             <div className="cart-price-detail total-sum">
                 <p className="cart-price-detail-left">Total</p>
-                <p className="cart-price-detail-right">$802</p>
+                <p className="cart-price-detail-right">
+                    ${getTotalCartAmount() + shipping}
+                </p>
             </div>
         </div>
     )
