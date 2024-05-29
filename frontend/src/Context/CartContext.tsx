5c9dccbcd485d952555d7ac83a39f7f0989b9d91
William Mattsson <wmattsson@hotmail.com>
2024-05-29 09:48:39 +0200

    Added add to cart functionality for logged in users and regular

--- frontend/src/Context/CartContext.tsx
+++ frontend/src/Context/CartContext.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useState, useEffect } from 'react'
 interface CartContextType {
     cartItems: Record<number, number>
     setCartItems: React.Dispatch<React.SetStateAction<Record<number, number>>>
+    addToCart: (productId: number) => void
 }
 
 export const CartContext = createContext<CartContextType | null>(null)
@@ -41,9 +42,44 @@ const CartContextProvider = ({ children }: CartContextProps) => {
         }
     }, [])
 
+    const addToCart = (productId: number): void => {
+        // Produkterna lägga till i cartItems
+        setCartItems((prev) => {
+            const newCartItems = { ...prev }
+            if (newCartItems[productId]) {
+                newCartItems[productId]++
+            } else {
+                newCartItems[productId] = 1
+            }
+            return newCartItems
+        })
+
+        // Om användaren är inloggad, lägg till produkten i databasen. Läggs i tabellen cartItems
+        if (localStorage.getItem('token')) {
+            fetch('/cart/add', {
+                method: 'POST',
+                headers: {
+                    token: `${localStorage.getItem('token')}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ productId: productId })
+            })
+                .then((res) => res.json())
+                .then((data) => console.log(data))
+                .catch((error) =>
+                    console.error('Error adding product to cart:', error)
+                )
+        } else {
+            console.log('User is not logged in')
+            console.log('Non user added:', productId)
+            console.log('Cart items:', cartItems)
+        }
+    }
+
     const contextValue: CartContextType = {
         cartItems,
-        setCartItems
+        setCartItems,
+        addToCart
     }
 
     return (
