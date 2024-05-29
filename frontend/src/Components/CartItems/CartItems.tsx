525d4852cfcec6ceddb8ac226cd61aec5855c6b3
William Mattsson <wmattsson@hotmail.com>
2024-05-29 10:57:43 +0200

    Displaying cart items, info, and remove funcitonality

--- frontend/src/Components/CartItems/CartItems.tsx
+++ frontend/src/Components/CartItems/CartItems.tsx
@@ -1,41 +1,37 @@
 import './CartItems.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useContext } from 'react'
+import { CartContext } from '../../Context/CartContext'
+import useProducts from '../../Context/useProducts'
+import { Link } from 'react-router-dom'
 
 const CartItems = () => {
-    const products = [
-        {
-            id: 400,
-            name: 'Product 400',
-            price: 400,
-            gender: 'men',
-            category: 'tops',
-            image: 'https://via.placeholder.com/150'
-        },
-        {
-            id: 401,
-            name: 'Product 401',
-            price: 401,
-            gender: 'men',
-            category: 'tops',
-            image: 'https://via.placeholder.com/150'
-        },
-        {
-            id: 402,
-            name: 'Product 402',
-            price: 402,
-            gender: 'men',
-            category: 'tops',
-            image: 'https://via.placeholder.com/150'
-        }
-    ]
+    const { allProducts } = useProducts()
+    const { cartItems } = useContext(CartContext) || {
+        getDefaultCart: () => ({})
+    }
+    const { getTotalCartItems } = useContext(CartContext) || {
+        getTotalCartItems: () => 0
+    }
 
-    const cartItems = [400, 402]
+    const { removeFromCart } = useContext(CartContext) || {
+        removeFromCart: () => {}
+    }
+
+    const handleRemoveClick = (
+        e: React.MouseEvent<SVGSVGElement>,
+        productId: number
+    ) => {
+        e.preventDefault()
+        removeFromCart(productId)
+    }
+
     return (
         <>
             <div className="cart-items">
                 <h2 className="cart-title">
-                    Cart products ({cartItems.length} items)
+                    Cart products ({getTotalCartItems()} items)
                 </h2>
                 <div className="cart-items-titles">
                     <p>Products</p>
@@ -44,36 +40,52 @@ const CartItems = () => {
                     <p>Remove</p>
                 </div>
                 <div className="cart-items-items">
-                    {products.map((product) => {
-                        if (cartItems.includes(product.id)) {
+                    {allProducts.map((product) => {
+                        if (cartItems && cartItems[product.id] > 0) {
+                            const quantity = cartItems[product.id]
+
                             return (
-                                <div className="cart-item" key={product.id}>
-                                    <div className="cart-item-info">
-                                        <img
-                                            className="cart-item-image"
-                                            src={product.image}
-                                            alt={product.name}
-                                        />
-                                        <div className="cart-item-info-p-c">
-                                            <p className="cart-item-info-name">
-                                                {product.name}
-                                            </p>
-                                            <p className="cart-item-info-price">
-                                                ${product.price}
-                                            </p>
+                                <Link
+                                    to={`/products/${product.gender}/${product.name}`}
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: 'black'
+                                    }}
+                                >
+                                    <div className="cart-item" key={product.id}>
+                                        <div className="cart-item-info">
+                                            <img
+                                                className="cart-item-image"
+                                                src={product.image}
+                                                alt={product.name}
+                                            />
+                                            <div className="cart-item-info-p-c">
+                                                <p className="cart-item-info-name">
+                                                    {product.name}
+                                                </p>
+                                                <p className="cart-item-info-price">
+                                                    ${product.price}
+                                                </p>
+                                            </div>
                                         </div>
+                                        <p className="cart-item-info-quantity">
+                                            {quantity}
+                                        </p>
+                                        <p className="cart-item-info-total">
+                                            ${product.price * quantity}
+                                        </p>
+                                        <FontAwesomeIcon
+                                            onClick={(e) =>
+                                                handleRemoveClick(e, product.id)
+                                            }
+                                            className="cart-remove-icon"
+                                            icon={faXmark}
+                                        />
                                     </div>
-                                    <p className="cart-item-info-quantity">1</p>
-                                    <p className="cart-item-info-total">
-                                        ${product.price}
-                                    </p>
-                                    <FontAwesomeIcon
-                                        className="cart-remove-icon"
-                                        icon={faXmark}
-                                    />
-                                </div>
+                                </Link>
                             )
                         }
+                        return null
                     })}
                 </div>
             </div>
