83b5e5e834fbc93ff274116b67cf6c6e3d346c4d
William Mattsson <wmattsson@hotmail.com>
2024-05-30 19:47:11 +0200

    Added form for /addproduct and product list for /deleteproduct

--- frontend/src/Components/AdminAddForm/AdminAddForm.tsx
+++ frontend/src/Components/AdminAddForm/AdminAddForm.tsx
@@ -1,6 +1,149 @@
 import './AdminAddForm.css'
+import { useState } from 'react'
+import useProducts from '../../Context/useProducts'
+
 const AdminAddForm = () => {
-    return <div>AdminAddForm</div>
+    const { fetchAllProducts } = useProducts()
+
+    interface FormData {
+        productName: string
+        productPrice: string | number
+        productCategory: 'tops' | 'bottoms'
+        productGender: 'men' | 'woman'
+        productImage: string
+    }
+
+    const [formData, setFormData] = useState<FormData>({
+        productName: '',
+        productPrice: '',
+        productCategory: 'tops',
+        productGender: 'men',
+        productImage: ''
+    })
+
+    const handleInputChange = (
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log('Formdata:', formData)
+        const formDataWithNumberPrice = {
+            ...formData,
+            productPrice: Number(formData.productPrice)
+        }
+
+        console.log('Formdata: Number:', formDataWithNumberPrice)
+
+        const response = await fetch('/addproduct', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        const data = await response.json()
+        console.log(data)
+
+        fetchAllProducts()
+
+        setFormData({
+            productName: '',
+            productPrice: '',
+            productCategory: 'tops',
+            productGender: 'men',
+            productImage: ''
+        })
+    }
+    return (
+        <div className="add-product-form">
+            <div className="add-product-form">
+                <h2>Add Product</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="admin-input-container">
+                        <label>
+                            Product Name
+                            <input
+                                value={formData.productName}
+                                placeholder="Gucci Slides"
+                                type="text"
+                                onChange={handleInputChange}
+                                name="productName"
+                                required
+                            />
+                        </label>
+                    </div>
+
+                    <div className="admin-input-container">
+                        <label>
+                            Product Price
+                            <input
+                                value={formData.productPrice}
+                                placeholder="1000"
+                                type="number"
+                                onChange={handleInputChange}
+                                name="productPrice"
+                                required
+                            />
+                        </label>
+                    </div>
+
+                    <div className="admin-select-container">
+                        <div className="admin-select">
+                            <label>
+                                Product Category
+                                <select
+                                    value={formData.productCategory}
+                                    onChange={handleInputChange}
+                                    name="productCategory"
+                                    required
+                                >
+                                    <option value="tops">Tops</option>
+                                    <option value="bottoms">Bottoms</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className="admin-select">
+                            <label>
+                                Product Gender
+                                <select
+                                    value={formData.productGender}
+                                    onChange={handleInputChange}
+                                    name="productGender"
+                                    required
+                                >
+                                    <option value="men">Men</option>
+                                    <option value="woman">Woman</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div className="admin-input-container">
+                        <label>
+                            Product Image
+                            <input
+                                value={formData.productImage}
+                                placeholder="https://placehold.co/600x400/EEE/31343C"
+                                type="text"
+                                onChange={handleInputChange}
+                                name="productImage"
+                                required
+                            />
+                        </label>
+                    </div>
+                    <button type="submit">Add Product</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default AdminAddForm
--- frontend/src/Components/AdminEditProducts/AdminEditProducts.tsx
+++ frontend/src/Components/AdminEditProducts/AdminEditProducts.tsx
@@ -1,6 +1,96 @@
 import './AdminEditProducts.css'
+import useProducts from '../../Context/useProducts'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
+
+interface Product {
+    id: number
+    name: string
+    price: number
+    category: string
+    gender: string
+    image: string
+}
+
 const AdminEditProducts = () => {
-    return <div>AdminEditProducts</div>
+    const { allProducts } = useProducts()
+
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        setProducts(allProducts)
+    }, [allProducts])
+
+    const deleteProduct = async (productId: number) => {
+        try {
+            const response = await fetch('/deleteproduct', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ productId })
+            })
+            const data = await response.json()
+
+            setProducts(
+                products.filter((product: Product) => product.id !== productId)
+            )
+
+            console.log(data)
+        } catch (error) {
+            console.error('Error deleting product:', error)
+        }
+    }
+
+    return (
+        <>
+            <h2 className="admin-products-title">All products</h2>
+            <div className="admin-products-titles">
+                <p>ID</p>
+                <p>Image</p>
+                <p>Name</p>
+                <p>Gender</p>
+                <p>Category</p>
+                <p>Price</p>
+                <div>
+                    <p>Edit</p>
+                    <p>Delete</p>
+                </div>
+            </div>
+            <div className="admin-products">
+                {products.map((product) => (
+                    <div className="admin-product" key={product.id}>
+                        <p className="admin-product-id">#{product.id}</p>
+                        <div className="admin-product-image-container">
+                            <img
+                                className="admin-product-image"
+                                src={product.image}
+                                alt={product.name}
+                            />
+                        </div>
+                        <p className="admin-product-name">{product.name}</p>
+                        <p className="admin-product-gender">{product.gender}</p>
+                        <p className="admin-product-category">
+                            {product.category}
+                        </p>
+                        <p className="admin-product-price">${product.price}</p>
+                        <div className="admin-products-icons">
+                            <div className="admin-product-edit">
+                                <FontAwesomeIcon icon={faPenToSquare} />
+                            </div>
+                            <div
+                                className="admin-product-delete"
+                                onClick={() => deleteProduct(product.id)}
+                            >
+                                <FontAwesomeIcon icon={faTrash} />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default AdminEditProducts
