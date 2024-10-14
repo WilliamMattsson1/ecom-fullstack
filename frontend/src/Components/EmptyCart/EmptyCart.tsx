2b6921cdba729cbbdba8cb5f70f01a111e133aa3
William Mattsson <william.mattsson@iths.se>
2024-10-14 14:05:19 +0200

    Made different views for empty card depending on if user is logged in

--- frontend/src/Components/EmptyCart/EmptyCart.tsx
+++ frontend/src/Components/EmptyCart/EmptyCart.tsx
@@ -2,25 +2,54 @@ import './EmptyCart.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const EmptyCart = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+        localStorage.getItem('token') ? true : false
+    )
+
+    useEffect(() => {
+        setIsLoggedIn(localStorage.getItem('token') ? true : false)
+        console.log('Här körs useEffect i EmptyCart, isLoggedIn:', isLoggedIn)
+    }, [isLoggedIn])
     return (
-        <div className="empty-cart-container">
-            <FontAwesomeIcon
-                className="empty-cart-icon"
-                icon={faCartShopping}
-            />
-            <h2>Your cart is empty</h2>
-            <p>Login to get ur saved items or go to all products.</p>
-            <div className="empty-cart-btn-container">
-                <Link to="/login">
-                    <button className="empty-cart-btn-left">Login</button>
-                </Link>
-                <Link to="/products">
-                    <button className="empty-cart-btn-right">Products</button>
-                </Link>
-            </div>
-        </div>
+        <>
+            {isLoggedIn ? (
+                <div className="empty-cart-container">
+                    <FontAwesomeIcon
+                        className="empty-cart-icon"
+                        icon={faCartShopping}
+                    />
+                    <h2>Your cart is empty</h2>
+                    <p>Go to all products to find something you like.</p>
+                    <Link to="/products">
+                        <button className="empty-cart-btn">Products</button>
+                    </Link>
+                </div>
+            ) : (
+                <div className="empty-cart-container">
+                    <FontAwesomeIcon
+                        className="empty-cart-icon"
+                        icon={faCartShopping}
+                    />
+                    <h2>Your cart is empty</h2>
+                    <p>Login to get ur saved items or go to all products.</p>
+                    <div className="empty-cart-btn-container">
+                        <Link to="/login">
+                            <button className="empty-cart-btn-left">
+                                Login
+                            </button>
+                        </Link>
+                        <Link to="/products">
+                            <button className="empty-cart-btn-right">
+                                Products
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
