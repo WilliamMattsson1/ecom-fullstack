cd1181514a610fb131ef120fc42717e19b171344
William Mattsson <william.mattsson@iths.se>
2024-10-14 13:37:51 +0200

    Added link to card from toast

--- frontend/src/Components/PopupCartAdded/PopupCartAdded.tsx
+++ frontend/src/Components/PopupCartAdded/PopupCartAdded.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import './PopupCartAdded.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faX } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 interface PopupCartAddedProps {
     product: string
@@ -20,13 +21,31 @@ const PopupCartAdded = ({ product, onClose }: PopupCartAddedProps) => {
     }, [onClose])
 
     return (
-        <div className="popup-cart-status-container">
-            <div className="popup-cart-text">
-                <FontAwesomeIcon className="popup-icon" icon={faCheck} />{' '}
-                <p>{product} added to cart</p>
+        <>
+            <div className="popup-cart-status-container">
+                <Link
+                    to={'/cart'}
+                    style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                        cursor: 'pointer'
+                    }}
+                >
+                    <div className="popup-cart-text">
+                        <FontAwesomeIcon
+                            className="popup-icon"
+                            icon={faCheck}
+                        />{' '}
+                        <p>{product} added to cart</p>
+                    </div>
+                </Link>
+                <FontAwesomeIcon
+                    className="x-icon"
+                    icon={faX}
+                    onClick={onClose}
+                />
             </div>
-            <FontAwesomeIcon className="x-icon" icon={faX} onClick={onClose} />
-        </div>
+        </>
     )
 }
 
